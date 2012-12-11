31fb36904fcaa89d3e9a7f732ec7e0944085cb87
Alan Jeffrey <ajeffrey@bell-labs.com>
2012-12-11 11:08:57 -0600

    Added deferred-worker.js.

---
+++ src/main/js/deferred-worker-task.js
@@ -0,0 +1,33 @@
+importScripts("require.js");
+
+function succ(id,results) {
+  try {
+    self.postMessage({ id: id, results: results });
+  } catch (exn) {
+    fail(id,[exn]);
+  }
+}
+
+function fail(id,failures) {
+  try {
+    self.postMessage({ id: id, failures: failures });
+  } catch (exn) { 
+    failures = failures.map(function(failure) { return failure.toString(); });
+    self.postMessage({ id: id, failures: failures });
+  }
+}
+
+self.addEventListener("message",function(event) {
+  var id = event.data.id;
+  var module = event.data.module;
+  var args = event.data.args;
+  require([module],function(fun) {
+    try {
+      fun.apply(self,args)
+        .done(function() { succ(id,Array.prototype.slice.call(arguments)); })
+        .fail(function() { fail(id,Array.prototype.slice.call(arguments)); });
+    } catch(exn) {
+      fail(id,[exn]);
+    }
+  });
+});
---
+++ src/main/js/deferred-worker.js
@@ -0,0 +1,28 @@
+define(["deferred"],function(deferred) {
+
+  return function(module) {
+    var worker = new Worker("js/deferred-worker-task.js");
+    var ids = 0;
+    var results = {};
+    worker.addEventListener("message",function(event) {
+      var result = results[event.data.id];
+      if (result) { 
+        delete results[event.data.id];
+        if (event.data.hasOwnProperty("results")) {
+          result.resolve.apply(result,event.data.results);
+        } else {
+          result.reject.apply(result,event.data.failures);
+        }
+      }
+    });
+    return function() {
+      var id = ids++;
+      var result = deferred.Deferred();
+      var args = Array.prototype.slice.call(arguments);
+      results[id] = result;
+      worker.postMessage({ id: id, module: module, args: args });
+      return result;
+    };
+  };
+
+});
