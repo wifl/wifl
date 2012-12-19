8fbf333acd46962c2922b7d75ebb7fefe526e857
Alan Jeffrey <ajeffrey@bell-labs.com>
2012-12-19 13:39:43 -0600

    Added support for browsers which don't have Web Workers.

--- src/main/js/deferred-worker.js
+++ src/main/js/deferred-worker.js
@@ -1,29 +1,50 @@
 define(["deferred"],function(deferred) {
 
-  return function(module) {
-    var workerURL = require.toUrl("deferred-worker-task.js");
-    var worker = new Worker(workerURL);
-    var ids = 0;
-    var results = {};
-    worker.addEventListener("message",function(event) {
-      var result = results[event.data.id];
-      if (result) { 
-        delete results[event.data.id];
-        if (event.data.hasOwnProperty("results")) {
-          result.resolve.apply(result,event.data.results);
-        } else {
-          result.reject.apply(result,event.data.failures);
+  if (this.Worker) {
+
+    // If the browser has support for Web Workers, then
+    // we fork off a worker for each module.
+    return function(module) {
+      var workerURL = require.toUrl("deferred-worker-task.js");
+      var worker = new Worker(workerURL);
+      var ids = 0;
+      var results = {};
+      worker.addEventListener("message",function(event) {
+        var result = results[event.data.id];
+        if (result) { 
+          delete results[event.data.id];
+          if (event.data.hasOwnProperty("results")) {
+            result.resolve.apply(result,event.data.results);
+          } else {
+            result.reject.apply(result,event.data.failures);
+          }
         }
-      }
-    });
-    return function() {
-      var id = ids++;
-      var result = deferred.Deferred();
-      var args = Array.prototype.slice.call(arguments);
-      results[id] = result;
-      worker.postMessage({ id: id, module: module, args: args });
-      return result;
+      },false);
+      return function() {
+        var id = ids++;
+        var result = deferred.Deferred();
+        var args = Array.prototype.slice.call(arguments);
+        results[id] = result;
+        worker.postMessage({ id: id, module: module, args: args });
+        return result;
+      };
     };
-  };
+
+  } else {
+
+    // If there is no support for Web Workers, we just
+    // run the module in the main thread.
+    return function(module) {
+      var result = deferred.Deferred();
+      require([module],function(fun) { result.resolve(fun); })
+      return function() {
+        var args = arguments;
+        return result.pipe(function(fun) {
+          return fun.apply(this,args);
+        });
+      };
+    }
+
+  }
 
 });
