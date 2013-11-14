808a59524b43f1851a28f25cc391a41af1e8ba89
pdanielsen <pdanielsen@alcatel-lucent.com>
2013-11-14 10:41:52 -0600

    removed -SNAPSHOT from version

--- examples/store/grails-app/conf/BuildConfig.groovy
+++ examples/store/grails-app/conf/BuildConfig.groovy
@@ -32,10 +32,10 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
 
         // add repo for wifl plugin
-        def wiflBase = System.properties["wifl.repository.url"]
+        def wiflBase = System.properties["wifl.repository.url"] ?: "http://wifl.org/ivy"
         def wiflResolver = new org.apache.ivy.plugins.resolver.URLResolver()
-        wiflResolver.addArtifactPattern(wiflBase + "[organisation]/[module]/[artifact]-[revision].[ext]")
-        wiflResolver.addIvyPattern(wiflBase + "[organisation]/[module]/[artifact]-[revision].xml") 
+        wiflResolver.addArtifactPattern(wiflBase + "/[organisation]/[module]/[artifact]-[revision].[ext]")
+        wiflResolver.addIvyPattern(wiflBase + "/[organisation]/[module]/[artifact]-[revision].xml") 
         wiflResolver.name = "wifl-repository" 
         wiflResolver.changingPattern = ".*-SNAPSHOT"
         wiflResolver.checkmodified = true
@@ -58,7 +58,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
         runtime "org.grails.plugins:jaxrs:0.7"
-        runtime "org.wifl:grails-wifl:0.3-SNAPSHOT"
+        runtime "org.wifl:grails-wifl:0.3"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
--- grails-wifl-plugin/WiflGrailsPlugin.groovy
+++ grails-wifl-plugin/WiflGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import org.wifl.grails.ConfigHelper
 
 class WiflGrailsPlugin {
     // the plugin version
-    def version = "0.3-SNAPSHOT"
+    def version = "0.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
